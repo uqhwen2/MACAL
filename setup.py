3bfd2e6842ebfa60fb0a86b5e3728d961c496ac1
dumi_dudu <103487148+uqhwen2@users.noreply.github.com>
2024-05-23 18:09:41 +1000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="causal-bald",
+    version="0.0.0",
+    description="BALD for causal-effect estimation",
+    long_description_content_type="text/markdown",
+    url="https://github.com/[anon]/causal-bald",
+    license="Apache-2.0",
+    packages=find_packages(),
+    install_requires=[
+        "click==8.0.1",
+        "torch==1.10.0",
+        "numpy==1.21.2",
+        "scipy==1.7.1",
+        "pandas==1.3.4",
+        "pyreadr==0.4.2",
+        "gpytorch==1.5.1",
+        "seaborn==0.11.2",
+        "hyperopt==0.2.5",
+        "ray[tune]==1.7.0",
+        "ray[default]==1.7.0",
+        "matplotlib==3.4.3",
+        "tensorboard==2.7.0",
+        "torchvision==0.11.1",
+        "scikit-learn==0.24.2",
+        "pytorch-ignite==0.4.7",
+    ],
+    entry_points={"console_scripts": ["causal-bald=causal_bald.application.main:cli"],},
+)
