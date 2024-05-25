e79f29889beb75e4e195ce148312e5cba7ac6df4
dumi_dudu <103487148+uqhwen2@users.noreply.github.com>
2024-05-25 16:48:03 +1000

    Update DeepGPR_TrueBMDAL-bait-DUE.sh

--- CMNIST/DeepGPR_TrueBMDAL-bait-DUE.sh
+++ CMNIST/DeepGPR_TrueBMDAL-bait-DUE.sh
@@ -4,6 +4,9 @@ for i in 0
 do
 	for j in $(seq 0 9)
 	do
-    	CUDA_VISIBLE_DEVICES=0 python DeepGPR_TrueBMDAL-DUE.py --seed $j --bmdal bait
+    	CUDA_VISIBLE_DEVICES=0 \
+     	python DeepGPR_TrueBMDAL-DUE.py \
+      	--seed $j \
+       	--bmdal bait
 	done
 done
