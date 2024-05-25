eeb179e11fd08259dafc748eb49f32478939fa8d
dumi_dudu <103487148+uqhwen2@users.noreply.github.com>
2024-05-25 16:39:11 +1000

    Update DeepGPR_TrueMU-DUE.sh

--- IBM/DeepGPR_TrueMU-DUE.sh
+++ IBM/DeepGPR_TrueMU-DUE.sh
@@ -4,6 +4,6 @@ for i in 0
 do
 	for j in $(seq 0 9)
 	do
-    	CUDA_VISIBLE_DEVICES=1 python causal_bald_due.py --seed $j --causalbald mu
+    	CUDA_VISIBLE_DEVICES=0 python causal_bald_due.py --seed $j --causalbald mu
 	done
 done
