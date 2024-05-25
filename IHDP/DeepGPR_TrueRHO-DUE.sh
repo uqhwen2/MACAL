0ca46cc039bf8bcec1b273c25994ee6569b27346
dumi_dudu <103487148+uqhwen2@users.noreply.github.com>
2024-05-25 16:44:12 +1000

    Update DeepGPR_TrueRHO-DUE.sh

--- IHDP/DeepGPR_TrueRHO-DUE.sh
+++ IHDP/DeepGPR_TrueRHO-DUE.sh
@@ -4,6 +4,9 @@ for i in 0
 do
 	for j in $(seq 0 99)
 	do
-    	python DeepGPR_TrueCausalBald-DUE.py --seed $j --causalbald rho
+    	CUDA_VISIBLE_DEVICES=0 \
+        python DeepGPR_TrueCausalBald-DUE.py \
+	--seed $j \
+        --causalbald rho
 	done
 done
