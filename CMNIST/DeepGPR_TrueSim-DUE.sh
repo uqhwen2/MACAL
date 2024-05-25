85f7ea1e70a37461c4b7f27e9cb0616c0b6d7886
dumi_dudu <103487148+uqhwen2@users.noreply.github.com>
2024-05-25 16:41:20 +1000

    Update DeepGPR_TrueSim-DUE.sh

--- CMNIST/DeepGPR_TrueSim-DUE.sh
+++ CMNIST/DeepGPR_TrueSim-DUE.sh
@@ -4,7 +4,7 @@ for i in 0
 do
 	for j in $(seq 0 9)
 	do
-    	CUDA_VISIBLE_DEVICES=3 \
+    	CUDA_VISIBLE_DEVICES=0 \
         python DeepGPR_TrueSim-DUE.py \
         --seed $j \
         --alpha 2.5
