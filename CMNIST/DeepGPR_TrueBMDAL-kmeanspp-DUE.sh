da623a4bdf4a4b61b466584d71ba697a8e790b5b
dumi_dudu <103487148+uqhwen2@users.noreply.github.com>
2024-05-25 16:40:13 +1000

    Update DeepGPR_TrueBMDAL-kmeanspp-DUE.sh

--- CMNIST/DeepGPR_TrueBMDAL-kmeanspp-DUE.sh
+++ CMNIST/DeepGPR_TrueBMDAL-kmeanspp-DUE.sh
@@ -4,6 +4,6 @@ for i in 0
 do
 	for j in $(seq 0 9)
 	do
-    	CUDA_VISIBLE_DEVICES=2 python DeepGPR_TrueBMDAL-DUE.py --seed $j --bmdal kmeanspp
+    	CUDA_VISIBLE_DEVICES=0 python DeepGPR_TrueBMDAL-DUE.py --seed $j --bmdal kmeanspp
 	done
 done
