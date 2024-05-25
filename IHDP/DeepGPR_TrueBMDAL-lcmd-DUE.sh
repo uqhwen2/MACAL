2c3caaf55ad7c439fdac1a82080cb41c080c8b5b
dumi_dudu <103487148+uqhwen2@users.noreply.github.com>
2024-05-25 16:46:14 +1000

    Update DeepGPR_TrueBMDAL-lcmd-DUE.sh

--- IHDP/DeepGPR_TrueBMDAL-lcmd-DUE.sh
+++ IHDP/DeepGPR_TrueBMDAL-lcmd-DUE.sh
@@ -4,7 +4,8 @@ for i in 0
 do
 	for j in $(seq 0 99)
 	do
-    	CUDA_VISIBLE_DEVICES=0 \python DeepGPR_TrueBMDAL-DUE.py \
+    	CUDA_VISIBLE_DEVICES=0 \
+     	python DeepGPR_TrueBMDAL-DUE.py \
         --seed $j \
 	--bmdal lcmd \
  	--selwithtrain True
