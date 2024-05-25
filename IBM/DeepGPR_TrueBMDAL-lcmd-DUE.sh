841a100298460725b4242101d447722d19f95424
dumi_dudu <103487148+uqhwen2@users.noreply.github.com>
2024-05-25 16:39:00 +1000

    Update DeepGPR_TrueBMDAL-lcmd-DUE.sh

--- IBM/DeepGPR_TrueBMDAL-lcmd-DUE.sh
+++ IBM/DeepGPR_TrueBMDAL-lcmd-DUE.sh
@@ -4,7 +4,7 @@ for i in 0
 do
 	for j in $(seq 0 9)
 	do
-    	CUDA_VISIBLE_DEVICES=1 \
+    	CUDA_VISIBLE_DEVICES=0 \
         python DeepGPR_TrueBMDAL-DUE.py \
         --seed $j \
         --bmdal lcmd \
