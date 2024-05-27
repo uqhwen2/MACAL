17b3efe5c6c14aad5b4dba764af420fbf0141c49
dumi_dudu <103487148+uqhwen2@users.noreply.github.com>
2024-05-27 19:04:39 +1000

    Update DeepGPR_TrueSim-DUE-MAX.py

--- IHDP/DeepGPR_TrueSim-DUE-MAX.py
+++ IHDP/DeepGPR_TrueSim-DUE-MAX.py
@@ -30,6 +30,7 @@ with open('experiments/config_sim_{}.json'.format(args.alpha), 'r') as file:
     config = json.load(file)
 
 from torch.utils.data import Dataset
+from sklearn.manifold import TSNE
 
 from models.utils import train_test_splitting
 
